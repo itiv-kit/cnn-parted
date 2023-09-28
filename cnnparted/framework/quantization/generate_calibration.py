57ec0cbf914989a684024a05aad0c9cac894a38d
Fabian Kreß <fabian.kress@kit.edu>
2023-09-28 10:16:28 +0200

    Add basic training

--- cnnparted/framework/quantization/generate_calibration.py
+++ cnnparted/framework/quantization/generate_calibration.py
@@ -1,20 +1,18 @@
 import torch.nn as nn
 
-from model_explorer.utils.setup import build_dataloader_generators, setup_torch_device
+from model_explorer.utils.setup import setup_torch_device
 from model_explorer.models.quantized_model import QuantizedModel
+from model_explorer.utils.data_loader_generator import DataLoaderGenerator
 
 from pytorch_quantization.tensor_quant import QuantDescriptor
 
-def generate_calibration(model : nn.Module, config : dict, progress : bool, filename : str):
-    dataloaders = build_dataloader_generators(config['datasets'])
+def generate_calibration(model : nn.Module, dataloader_gen : DataLoaderGenerator, progress : bool, filename : str):
     device = setup_torch_device()
-
-    dataset_gen = dataloaders['calibrate']
 
     quant_descriptor = QuantDescriptor(calib_method='histogram')
     qmodel = QuantizedModel(model, device, quantization_descriptor=quant_descriptor)
 
-    qmodel.generate_calibration_file(dataset_gen.get_dataloader(), progress, calib_method='histogram',
+    qmodel.generate_calibration_file(dataloader_gen.get_dataloader(), progress, calib_method='histogram',
                                      method='percentile', percentile=99.99)
 
     qmodel.save_parameters(filename)
