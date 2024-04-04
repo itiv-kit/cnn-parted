b5323e47b525d950d31d6f0050ae80a627dbbea6
Fabian Kreß <fabian.kress@kit.edu>
2024-04-05 00:18:07 +0200

    Update examples

--- cnnparted/workloads/fcnresnet50.py
+++ cnnparted/workloads/fcnresnet50.py
@@ -1,8 +1,6 @@
 from torchvision import models
-
 from model_explorer.accuracy_functions.segmentation_accuracy import compute_sematic_segmentation_accuracy
 
-# Simply take the FCN-ResNet-50 available in torchvision
-model = models.segmentation.fcn_resnet50(weights=models.segmentation.FCN_ResNet50_Weights.DEFAULT)
-
+# Simply take the model available in torchvision
+model = models.segmentation.fcn_resnet50(weights=models.segmentation.FCN_ResNet50_Weights.COCO_WITH_VOC_LABELS_V1)
 accuracy_function = compute_sematic_segmentation_accuracy
--- cnnparted/workloads/mobilenetv3.py
+++ cnnparted/workloads/mobilenetv3.py
@@ -2,5 +2,5 @@ from torchvision import models
 from model_explorer.accuracy_functions.classification_accuracy import compute_classification_accuracy
 
 # Simply take the model available in torchvision
-model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+model = models.mobilenet_v3_large(weights=models.MobileNet_V3_Large_Weights.IMAGENET1K_V2)
 accuracy_function = compute_classification_accuracy
