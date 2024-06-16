004741411e79d05e2e08d09b655d6ab9eb0748cb
rh8588 <rh8588@kit.edu>
2024-06-16 21:24:02 +0200

    Update init file with new architecture mutators

--- cnnparted/framework/__init__.py
+++ cnnparted/framework/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['ConfigHelper', 'NodeThread', 'GraphAnalyzer', 'PartitioningOptimizer', 'RobustnessOptimizer', 'AccuracyEvaluator', 'GemminiArchitectureMutator', 'ArchitectureMutator', 'ArchitectureConfig']
+__all__ = ['ConfigHelper', 'NodeThread', 'GraphAnalyzer', 'PartitioningOptimizer', 'RobustnessOptimizer', 'AccuracyEvaluator', 
+           'GemminiArchitectureMutator', 'EyerissArchitectureMutator', 'SimbaArchitectureMutator', 'ArchitectureMutator', 'ArchitectureConfig']
 
 from framework.helpers.ConfigHelper import ConfigHelper
 from framework.node.NodeThread import NodeThread
@@ -7,5 +8,7 @@ from framework.optimizer.PartitioningOptimizer import PartitioningOptimizer
 from framework.optimizer.RobustnessOptimizer import RobustnessOptimizer
 from framework.quantization.AccuracyEvaluator import AccuracyEvaluator
 from framework.dse.GemminiArchitectureMutator import GemminiArchitectureMutator
+from framework.dse.EyerissArchitectureMutator import EyerissArchitectureMutator
+from framework.dse.SimbaArchitectureMutator import SimbaArchitectureMutator
 from framework.dse.ArchitectureMutator import ArchitectureMutator, ArchitectureConfig
 
