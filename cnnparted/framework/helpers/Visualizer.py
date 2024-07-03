Update visualizer to plot bar chart based on DataFrame Fix calculation of system area in PartitioningProblem Add additional plots after DSE Fix pruning of accelerator design space

cnnparted/framework/helpers/Visualizer.py
@@ -2,10 +2,11 @@ import os
 import matplotlib.pyplot as plt
 from typing import Dict, List
 import numpy as np
+import pandas as pd
 
 from framework.helpers.DesignMetrics import calc_metric, get_metric_info
 
-def plotMetricPerConfigPerLayer(stats: Dict, dir: str, metric: str, type: str = "line"):
+def plotMetricPerConfigPerLayer(stats: Dict, dir: str, metric: str, type: str = "line", scale: str = "linear", prefix: str = ""):
     assert type in ["line", "bar"], "Currently only 'line' and 'bar' are supported as plot types"
 
     metric = metric.lower()
@@ -32,6 +33,7 @@ def plotMetricPerConfigPerLayer(stats: Dict, dir: str, metric: str, type: str = 
     plt.figure(dpi=1200)
     plt.xlabel("Layer Number")
     plt.ylabel(metric_str)
+    plt.yscale(scale)
     plt.title(f"{metric_str} for all Designs by layer")
     plt.gca().set_prop_cycle(marker=["o", "+", "*", "s", "x", "d"], 
                             color=['#1b9e77','#d95f02','#7570b3','#e7298a','#66a61e','#e6ab02'])
@@ -41,13 +43,13 @@ def plotMetricPerConfigPerLayer(stats: Dict, dir: str, metric: str, type: str = 
         for i in range(len(metric_per_design)): 
             plt.plot(layer_idx, metric_per_design[i], label = labels[i], linestyle="dotted")
     elif type == "bar":
-        width = (0.5)/len(metric_per_design)
-        multiplier = 0
-        for i in range(len(metric_per_design)):
-            offset = width * multiplier
-            plt.bar(layer_idx+offset, metric_per_design[i], width=width, label=labels[i])
-            multiplier += 1
+        data = np.array(metric_per_design).T
+        df = pd.DataFrame(data, columns=labels)
+        df.plot.bar(color=['#1b9e77','#d95f02','#7570b3','#e7298a','#66a61e','#e6ab02'])
 
     plt.legend()
-    plt.savefig(os.path.join(dir, metric+"_"+type+".png"))
+    
+    fname = prefix + metric + "_" + type + ".png"
+    plt.savefig(os.path.join(dir, fname))
+    plt.close()
     

cnnparted/framework/node/Timeloop.py
@@ -102,15 +102,39 @@ class Timeloop:
                 self.runroot = os.path.join(*self.runroot.split(os.path.sep)[:-1]) # this removes 'designI' from path
                 i += 1
             
-            # Decide which designs to further evaluate
-            # Rough pruning based on EAP
-            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "edap", type="bar")
             plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "edap")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "edap", scale="log", prefix="log_")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "edap", type="bar")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "edap", type="bar", scale="log", prefix="log_")
+
             plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "edp")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "edp", scale="log", prefix="log_")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "edp", type="bar")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "edp", type="bar", scale="log", prefix="log_")
+
             plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "power_density")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "power_density", scale="log", prefix="log_")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "power_density", type="bar")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "power_density", type="bar", scale="log", prefix="log_")
+
             plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "eda2p")
-            # after prune, stats is still a dict
-            pruned_stats = self._prune_accelerator_designs(deepcopy(stats), 2, "edap")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "eda2p", scale="log", prefix="log_")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "eda2p", type="bar")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "eda2p", type="bar", scale="log", prefix="log_")
+
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "energy")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "energy", scale="log", prefix="log_")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "energy", type="bar")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "energy", type="bar", scale="log", prefix="log_")
+
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "eap")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "eap", scale="log", prefix="log_")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "eap", type="bar")
+            plotMetricPerConfigPerLayer(stats, self.tl_cfg["work_dir"], "eap", type="bar", scale="log", prefix="log_")
+
+            # Decide which designs to further evaluate. After pruning, stats is still a dict
+            # Then, turn the dict into a list
+            pruned_stats = self._prune_accelerator_designs(deepcopy(stats), 100, "edap")
             for k, d in  pruned_stats.items():
                 d["tag"] = k
                 self.stats.append(d)
@@ -211,11 +235,8 @@ class Timeloop:
         design_candidates = np.unique(design_candidates) 
 
         # Remove all designs that have not been found to be suitable design candidates
-        for tag, design in stats.items():
-            if tag not in design_candidates:
-                stats.pop(tag)
-
-        return stats
+        pruned_stats = {tag: design for tag, design in stats.items() if tag in design_candidates}
+        return pruned_stats
 
 
     def _rewrite_mapper_cfg(self, src : str, dst : str) -> None:

cnnparted/framework/optimizer/PartitioningProblem.py
@@ -56,7 +56,7 @@ class PartitioningProblem(ElementwiseProblem):
         elif self.fixed_sys and not np.array_equal(np.sort(p[-self.num_pp-1:]), p[-self.num_pp-1:]): # keep order of Accelerators
             valid = False
         else:
-            design_id = [] #should be list of len num_platform - num_links
+            design_id = {}
             th_pp = []
             partitions = []
             part_latency = deque()
@@ -68,7 +68,9 @@ class PartitioningProblem(ElementwiseProblem):
                 v, optimal_design_id, mem[i-self.num_pp-1] = self._eval_partition(p[i], last_pp, pp, l_pp, e_pp, successors)
                 valid &= v
 
-                design_id.append(optimal_design_id)
+                current_platform = list(self.nodeStats.keys())[p[i]-1]
+                design_id[current_platform] = optimal_design_id
+                #design_id.append(optimal_design_id)
                 partitions.append([p[i], last_pp, pp])
 
                 # evaluate link
@@ -107,6 +109,7 @@ class PartitioningProblem(ElementwiseProblem):
             energy = sum(e_pp) + link_energy
             throughput = self._get_throughput(th_pp, part_latency) * -1
             area = self._get_area(partitions, design_id)
+            design_id = list(design_id.values())
 
         out["F"] = [latency, energy, throughput, area, link_latency, link_energy] #+ list(bandwidth) #+ list(mem)
 
@@ -190,11 +193,12 @@ class PartitioningProblem(ElementwiseProblem):
         # d0 | ...| ...| ...| ...|
         # d1 | ...| ...| ...| ...|
         edp_per_design_per_layer = np.multiply(np.array(energy_per_design), np.array(latency_per_design))
-        edap_per_design_per_layer = np.multiply(edp_per_design_per_layer, np.array(area_per_design))
+        #edap_per_design_per_layer = np.multiply(edp_per_design_per_layer, np.array(area_per_design))
 
     	# Use Energy-Delay-Area-Product as criterium for optimality
         # edap_per_design is a column vector, each row has the EDAP for one design
-        edap_per_design = np.sum(edap_per_design_per_layer, axis=1)
+        edap_per_design = np.multiply(np.sum(edp_per_design_per_layer, axis=1), np.array(area_per_design))
+        #edap_per_design = np.sum(edap_per_design_per_layer, axis=1)
         optimal_design_id = self._get_tag_from_id(platform, np.argmax(edap_per_design))
 
         l_pp.append(platform_latency_per_design[optimal_design_id])
@@ -274,17 +278,20 @@ class PartitioningProblem(ElementwiseProblem):
 
         area = 0.0
         for key in parts.keys():
-            platform = [*self.nodeStats][key]
+            #platform = [*self.nodeStats][key]
+            platform = list(self.nodeStats.keys())[key]
             if self.nodeStats[platform]['type'] == 'mnsim':
                 for part in parts[key]:
                     for l in self.schedule[part[0]:part[1]]:
                         if l in [*self.nodeStats[platform]]:
                             area += float(self.nodeStats[platform][l]['area'])
             else: # timeloop
+                #part: partition
                 for part in parts[key]:
-                    if part[0] != part[1]:
-                        first_layer = [*self.nodeStats[platform]["eval"][0]["layers"]][0]
-                        area += float(self.nodeStats[platform]["eval"][0]["layers"][first_layer]['area']) #TODO This should be adapted to account for different designs
+                    if part[0] != part[1]: #partitions stores [platform, layer_ids], here check if both partitions are executed on same platform
+                        id = design_id[platform]
+                        first_layer = [*self.nodeStats[platform]["eval"][id]["layers"]][0]
+                        area += float(self.nodeStats[platform]["eval"][id]["layers"][first_layer]['area'])
                         break
 
         return area