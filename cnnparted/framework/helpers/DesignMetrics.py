Add return type to calc_metrics function Update metric calculation during accelerator pruning Fix reading and writing of CSV files with Timeloop results Pruning of accelerator designs is now done in NodeThread.py, no longer in Timeloop.py Timeloop runs with DSE no longer save architecture info Fix layer names when running Timeloop with DSE

cnnparted/framework/helpers/DesignMetrics.py
@@ -2,7 +2,7 @@ import numpy as np
 
 SUPPORTED_METRICS = ["edp", "eap", "edap", "eda2p", "latency", "energy", "area"]
 
-def calc_metric(energy_per_design: np.array, latency_per_design: np.array, area_per_design: np.array, metric: str, reduction = True):
+def calc_metric(energy_per_design: np.array, latency_per_design: np.array, area_per_design: np.array, metric: str, reduction = True) -> np.array:
     # function assumes that x_per_design arrays have this layout:
     #  x | l0 | l1 | l2 | l3 |
     # ------------------------

cnnparted/framework/node/NodeThread.py
@@ -1,7 +1,7 @@
 import os
 import csv
 import numpy as np
-from typing import Dict
+from typing import Dict, List
 
 from framework.ModuleThreadInterface import ModuleThreadInterface
 from framework.node.Timeloop import Timeloop
@@ -43,7 +43,7 @@ class NodeThread(ModuleThreadInterface):
         else:
             mn.run()
             self.stats["eval"] = [mn.stats]
-            self._write_layer_csv(fname_csv)
+            self._write_layer_csv(fname_csv, stats=mn.stats)
 
         self.stats['bits'] = mn.pim_realADCbit()
 
@@ -55,22 +55,24 @@ class NodeThread(ModuleThreadInterface):
         fname_csv = os.path.join(self.work_dir, self.runname + "_" + config["accelerator"] + "_tl_layers.csv") #runroot + "_tl_layers.csv"
 
         if os.path.isfile(fname_csv):
-            self.stats["eval"] = self._read_layer_csv(fname_csv)
+            read_stats = self._read_layer_csv(fname_csv)
+            pruned_stats = self._prune_accelerator_designs(read_stats, 1, "edap")
+            self.stats["eval"] =  pruned_stats
             return
 
         layers = self.ga.get_timeloop_layers()
         tl = Timeloop(config)
         tl.run(layers, self.progress)
 
-        write_stats = {}
-        write_stats["eval"] = tl.stats
-        self._write_layer_csv(fname_csv, stats=write_stats)
+        self._write_layer_csv(fname_csv, tl.stats)
+
+        pruned_stats = self._prune_accelerator_designs(tl.stats, 1, "edap")
 
         # Prune accelerator design space 
-        self.stats["eval"] = self._prune_accelerator_designs(tl.stats, 3, "edap")
+        self.stats["eval"] = pruned_stats
 
 
-    def _prune_accelerator_designs(self, stats: Dict, top_k: int, metric: str):
+    def _prune_accelerator_designs(self, stats: List[Dict], top_k: int, metric: str):
         # If there are less designs than top_k simply return the given list
         if len(stats) <= top_k:
             return stats
@@ -82,20 +84,27 @@ class NodeThread(ModuleThreadInterface):
         # d0 | ...| ...| ...| ...|
         # d1 | ...| ...| ...| ...|
         metric_per_design = []
-        labels = []
+        energy_per_design = []
+        latency_per_design = []
+        area_per_design = []
 
-        for tag, design in stats.items():
-            metric_per_layer = []
+        for design in stats:
+            tag = design["tag"]
             layers = design["layers"]
-            for key, layer in layers.items():
-                metric_per_layer.append(calc_metric(layer, metric))
+            energy_per_layer = []
+            latency_per_layer = []
+            for name, layer in layers.items():
+                energy_per_layer.append(layer["energy"])    
+                latency_per_layer.append(layer["latency"])    
 
-            labels.append(tag)
-            metric_per_design.append(metric_per_layer)
+            energy_per_design.append(energy_per_layer)
+            latency_per_design.append(latency_per_layer)
+            area_per_design.append([layer["area"]])
+
+        metric_per_design = calc_metric(np.array(energy_per_design), np.array(latency_per_design), np.array(area_per_design), metric, reduction=False)
 
         # Now, we need to find the top_k designs per layer
         design_candidates = []
-        metric_per_design = np.array(metric_per_design)
         for col in metric_per_design.T:
             metric_for_layer = col.copy()
             metric_for_layer = np.argsort(metric_for_layer)
@@ -106,35 +115,49 @@ class NodeThread(ModuleThreadInterface):
         design_candidates = np.unique(design_candidates) 
 
         # Remove all designs that have not been found to be suitable design candidates
-        pruned_stats = {tag: design for tag, design in stats.items() if tag in design_candidates}
+        pruned_stats = []
+        for design in stats:
+            if design["tag"] in design_candidates: 
+               tag = design["tag"] 
+               layers = design["layers"]
+               #arch_config = design["arch_config"]
+               pruned_stats.append({"tag": tag, "layers": layers})
+
         return pruned_stats
 
 
     def _read_layer_csv(self, filename: str) -> dict:
         designs = []
         stats = {}
+        first_design = True
 
         with open(filename, 'r', newline="") as f:
             reader = csv.DictReader(f, delimiter=";")
             current_design_tag = "design_0"
             stats["layers"] = {}
-            stats["tag"] = current_design_tag
+            #stats["tag"] = current_design_tag
             for row in reader:
                 if row["Design Tag"] != current_design_tag:
-                    current_design_tag = row["Design Tag"]
+                    stats["tag"] = current_design_tag if first_design else row["Design Tag"]
                     designs.append(stats)
+
+                    stats = {}
                     stats["layers"] = {}
-                    stats["tag"] = current_design_tag
+                    current_design_tag = row["Design Tag"]
+                    first_design = False
+
                 layer_name = row['Layer']
                 stats["layers"][layer_name] = {}
-                stats["layers"][layer_name]['latency'] = row['Latency [ms]']
-                stats["layers"][layer_name]['energy'] = row['Energy [mJ]']
-                stats["layers"][layer_name]['area'] = row['Area [mm2]']
+                stats["layers"][layer_name]['latency'] = float(row['Latency [ms]'])
+                stats["layers"][layer_name]['energy'] = float(row['Energy [mJ]'])
+                stats["layers"][layer_name]['area'] = float(row['Area [mm2]'])
 
+        # Append stats for final design
+        stats["tag"] = row["Design Tag"]
         designs.append(stats)
         return designs
 
-    def _write_layer_csv(self, filename: str, stats: Dict = None) -> None:
+    def _write_layer_csv(self, filename: str, stats: List[Dict] = None) -> None:
         if stats is None:
             stats = self.stats
         with open(filename, "w", newline="") as f:
@@ -149,7 +172,7 @@ class NodeThread(ModuleThreadInterface):
             ]
             writer.writerow(header)
             row_num = 1
-            for i, design in enumerate(stats["eval"]):
+            for i, design in enumerate(stats):
                 layers = design["layers"]
                 for layer in layers.keys():
                     if isinstance(design["layers"][layer], dict):

cnnparted/framework/optimizer/PartitioningProblem.py
@@ -71,7 +71,6 @@ class PartitioningProblem(ElementwiseProblem):
 
                 current_platform = list(self.nodeStats.keys())[p[i]-1]
                 design_id[current_platform] = optimal_design_id
-                #design_id.append(optimal_design_id)
                 partitions.append([p[i], last_pp, pp])
 
                 # link evaluation
@@ -127,9 +126,10 @@ class PartitioningProblem(ElementwiseProblem):
             energy_per_layer = []
             latency_per_layer = []
 
-            for j in range(last_pp + 1, pp + 1):
+            # TODO: Should this pp+1?
+            for j in range(last_pp + 1, pp):
                 layer = self.schedule[j-1]
-                #could be called for diff designs, then use best
+
                 layer_latency = self._get_layer_latency(platform, design_id, layer)
                 layer_energy = self._get_layer_energy(platform, design_id, layer)
 
@@ -143,11 +143,12 @@ class PartitioningProblem(ElementwiseProblem):
             energy_per_design.append(energy_per_layer)
             area_per_design.append([self._get_area_platform(platform, design_id)])
 
-            platform_energy_per_design.append(platform_latency)
+            platform_energy_per_design.append(platform_energy)
             platform_latency_per_design.append(platform_latency)
 
         # Check bit-width, analyze memory requirements and generate successors needed for link analysis
-        for j in range(last_pp + 1, pp + 1):
+        # TODO: Should this be pp+1?
+        for j in range(last_pp + 1, pp):
             valid &= self._check_layer_bitwidth(platform, layer)
             if layer in self.layer_params.keys():
                 part_l_params += self.layer_params[layer]
@@ -176,6 +177,10 @@ class PartitioningProblem(ElementwiseProblem):
         # Decide which design should be used
     	# Use Energy-Delay-Area-Product as criterium for optimality
         metric_per_design = calc_metric(np.array(energy_per_design), np.array(latency_per_design), np.array(area_per_design), "edap", reduction=True)
+        
+        # Every design is assigned an id during the DSE. Afterwards, they are reshaped to a list,
+        # the index in that list is given by design_id. After the pruning it is possible that
+        # design_id and design_tag are no longer the same, which is why we lookup the tag
         optimal_design_id = self._get_tag_from_id(platform, np.argmax(metric_per_design))
 
         l_pp.append(platform_latency_per_design[optimal_design_id])