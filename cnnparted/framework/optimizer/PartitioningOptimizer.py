acb86442a004a42a79becc70e8ce329c80056204
Fabian Kreß <fabian.kress@kit.edu>
2024-09-19 07:33:28 +0200

    [PartOpt] Adjust initial population

--- cnnparted/framework/optimizer/PartitioningOptimizer.py
+++ cnnparted/framework/optimizer/PartitioningOptimizer.py
@@ -128,6 +128,11 @@ class PartitioningOptimizer(Optimizer):
         samples = []
         rng = default_rng(seed=42)
 
+        for i in range(num_platforms):
+            pps = np.full(num_pp, 0)
+            accs = np.full(num_pp+1, i) * num_layers
+            samples.append(pps.tolist() + accs.tolist())
+
         while len(samples) < self.pop_size:
             pps = rng.integers(low=0, high=xu+1, size=num_pp)
             #pps = np.random.randint(0, xu+1, size=num_pp)
