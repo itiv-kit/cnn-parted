1f4c8454c2a0b4cdc8b4f07721c1009fd85505db
Fabian Kreß <fabian.kress@kit.edu>
2024-09-24 11:13:39 +0200

    [Robustness] Replace GA by SA

--- cnnparted/framework/optimizer/RobustnessOptimizer.py
+++ cnnparted/framework/optimizer/RobustnessOptimizer.py
@@ -1,111 +1,112 @@
 import os
+import torch
 import torch.nn as nn
 import numpy as np
-from numpy.random import default_rng
 import pandas as pd
 
-from pymoo.algorithms.moo.nsga2 import NSGA2
-from pymoo.operators.crossover.sbx import SBX
-from pymoo.operators.mutation.pm import PM
-from pymoo.operators.repair.rounding import RoundingRepair
-from pymoo.operators.sampling.rnd import IntegerRandomSampling
-from pymoo.optimize import minimize
-from pymoo.termination import get_termination
-from pymoo.core.callback import Callback
+
+from copy import deepcopy
+
+from model_explorer.utils.setup import build_dataloader_generators
+
+from framework.quantization.FaultyQuantizedModel import FaultyQuantizedModel
+from framework.quantization.generate_calibration import generate_calibration
+
+from scipy.optimize import dual_annealing
 
 from typing import Callable
 
 from framework.optimizer.Optimizer import Optimizer
-from framework.optimizer.RobustnessProblem import RobustnessProblem
 
-
-class RobustnessOptimizerCallback(Callback):
-    def __init__(self, schedule: list, steps: list) -> None:
-        super().__init__()
-        assert len(schedule) == len(steps)
-        self.schedule = schedule
-        self.steps = steps
-
-    def _update(self, algorithm: NSGA2):
-        print(f"Reached generation {algorithm.n_iter}")
-        if algorithm.n_iter in self.schedule:
-            new_sample_limit = self.steps[self.schedule.index(algorithm.n_iter)]
-            print(f"\tUpdating sample limit to {new_sample_limit}")
-            algorithm.problem.update_sample_limit(new_sample_limit)
 
 
 class RobustnessOptimizer(Optimizer):
     def __init__(self, work_dir: str, run_name: str, model: nn.Module, accuracy_function: Callable, config: dict, device : str, progress: bool):
+        rob_conf = config.get('robustness')
+        self.gpu_device = torch.device(device)
+
+        self.accuracy_function = accuracy_function
+
+        self.qmodel = FaultyQuantizedModel(model, self.gpu_device, same_bit_for_weight_and_input=True)
+        self.dataset_config = deepcopy(config['datasets'])
+
+        dataloaders = build_dataloader_generators(self.dataset_config)
+        calib_dataloadergen = dataloaders['calibrate']
+        self.val_dataloadergen = dataloaders['validation']
+
+        calib_conf = self.dataset_config.get('calibrate')
+        calibration_file = calib_conf.get('file')
+        if not os.path.exists(calibration_file):
+            generate_calibration(model, calib_dataloadergen, True, calibration_file, self.gpu_device)
+
+        self.qmodel.load_parameters_file(calibration_file)
+
+        self.n_var = self.qmodel.get_explorable_parameter_count()
+        self.bits = rob_conf.get('bits')
+        self.max_bits_idx = [len(self.bits) - 2, len(self.bits) - 1]
+
+        self.delta = rob_conf.get('delta')
+
         self.fname_csv = os.path.join(work_dir, run_name + "_" + "robustness.csv")
+        self.progress = progress
 
-        self.sample_limit_schedule = config.get('robustness').get('sample_limit_generation_schedule')
-        self.sample_limit_steps = config.get('robustness').get('sample_limit_steps')
-
-        self.pop_size = config.get('robustness').get('pop_size')
-        self.num_gen = config.get('robustness').get('num_gen')
-        self.problem = RobustnessProblem(model, self.sample_limit_steps[0], config, device, accuracy_function, progress)
-
-        self.max_bits_idx = [len(config.get('robustness').get('bits')) - 2, len(config.get('robustness').get('bits')) - 1]
-
-        self.delta = config.get('robustness').get('delta')
 
     def optimize(self):
-        rng = default_rng(seed=42)
-        n_var = self.problem.n_var
-        init_x = [np.full(n_var, self.max_bits_idx[-1]), np.full(n_var, self.max_bits_idx[-2])]
-        for i in range(self.pop_size-2):
-            init_x.append(rng.choice(self.max_bits_idx, size=n_var))
-
         if not os.path.isfile(self.fname_csv):
-            algorithm = NSGA2(
-                pop_size=self.pop_size,
-                n_offsprings=self.pop_size,
-                sampling=np.array(init_x),
-                crossover=SBX(prob=0.9, eta=5, repair=RoundingRepair()),
-                mutation=PM(prob=0.9, eta=10, repair=RoundingRepair()),
-                eliminate_duplicates=True)
+            acc_lut = {}
+            def f(x, ref):
+                layer_bit_nums = []
+                x = np.round(x).astype(int)
 
-            callback = RobustnessOptimizerCallback(self.sample_limit_schedule, self.sample_limit_steps)
-            res = minimize(self.problem,
-                           algorithm,
-                           termination=get_termination('n_gen', self.num_gen),
-                           seed=1,
-                           save_history=False,
-                           callback=callback,
-                           verbose=False)
+                if tuple(x) in [*acc_lut]:
+                    accuracy_result = acc_lut[tuple(x)]
+                    if self.progress:
+                        print("[RobustenessOptimizer] Skipping inference due to duplicate: ", accuracy_result)
+                else:
+                    for i in x:
+                        layer_bit_nums.append(self.bits[i])
+                    self.qmodel.bit_widths = layer_bit_nums
+                    self.qmodel.base_model.eval()
 
-            if not isinstance(res.X, np.ndarray):
-                print()
-                print("### [RobustnessOptimizer] No valid bitwidth combination found! ###")
-                print()
-                quit()
+                    accuracy_result = self.accuracy_function(
+                        self.qmodel.base_model.to(self.gpu_device),
+                        self.val_dataloadergen,
+                        progress=self.progress,
+                        title=f"Infere"
+                    )
+                    accuracy_result = float(accuracy_result.cpu().detach().numpy())
+                    acc_lut[tuple(x)] = accuracy_result
 
-            df = pd.DataFrame(res.X).replace(to_replace=range(0,len(self.problem.bits)), value=self.problem.bits)
-            res.X = df.to_numpy()
+                if ref == 0:
+                    return accuracy_result
+                else:
+                    return self.qmodel.get_bit_weighted() * abs(accuracy_result - ref)
 
-            data = self._get_paretos_int(res)
-            data = np.abs(data)
-            data = np.delete(data, np.s_[0], axis=1) # delete G column
+            init_x = np.full(self.n_var, self.max_bits_idx[-1])
+            acc_ref = f(init_x, 0) - self.delta
 
-            df = pd.DataFrame(data)
+            lw = np.full(self.n_var, 0)
+            up = np.full(self.n_var, self.max_bits_idx[-1])
+            _ = dual_annealing(f, args=[acc_ref], bounds=list(zip(lw, up)), maxiter=10, x0=init_x)
+
+            for key in acc_lut:
+                acc_lut[key] -= acc_ref
+                acc_lut[key] = abs(acc_lut[key])
+
+            res = min(acc_lut, key=acc_lut.get)
+            constr = [i for i in res]
+            constr.append(None)
+            constr.append(acc_lut[res])
+            constr.append(1)
+            df = pd.DataFrame(constr).replace(to_replace=range(0,len(self.bits)), value=self.bits).transpose()
             df.to_csv(self.fname_csv, header=False)
         else:
             df = pd.read_csv(self.fname_csv, header=None, index_col=0)
             data = df.to_numpy()
 
-        best_idx = np.argmax(data, axis=0)[-2]
-        lowest_accuracy = data[best_idx][-2] - self.delta
-
-        sel_idx = 0
-        sel_accuracy = data[best_idx][-2]
-        for i, x in enumerate(data):
-            if x[-2] >= lowest_accuracy and x[-2] < sel_accuracy:
-                sel_idx = i
-                sel_accuracy = x[-2]
-
-        constr = data[sel_idx][:-3]
+        data = df.to_numpy()[0]
         constr_dict = {}
-        for i, name in enumerate(self.problem.qmodel.explorable_module_names):
-            constr_dict[name] = constr[int(i/2)]
+        for i, name in enumerate(self.qmodel.explorable_module_names):
+            constr_dict[name] = data[int(i/2)]
 
         return constr_dict
