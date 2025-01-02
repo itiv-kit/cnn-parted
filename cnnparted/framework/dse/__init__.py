f19ed66341b07232dc7b5622f7ea49cde13cfbfb
rh8588 <rh8588@kit.edu>
2025-01-02 14:31:08 +0100

    Fix import of DSE classes

--- cnnparted/framework/dse/__init__.py
+++ cnnparted/framework/dse/__init__.py
@@ -1,10 +1,9 @@
-from framework.dse.eyeriss_architecture_mutator import EyerissArchitectureMutator 
-from framework.dse.gemmini_architecture_mutator import GemminiArchitectureMutator 
-from framework.dse.simba_architecture_mutator import SimbaArchitectureMutator 
+from framework.dse.eyeriss_architecture_mutator import EyerissArchitectureMutator
+from framework.dse.gemmini_architecture_mutator import GemminiArchitectureMutator
+from framework.dse.simba_architecture_mutator import SimbaArchitectureMutator
 
-__all__ = [     
-    EyerissArchitectureMutator,     
-    GemminiArchitectureMutator,     
-    SimbaArchitectureMutator 
+__all__ = [
+    EyerissArchitectureMutator,
+    GemminiArchitectureMutator,
+    SimbaArchitectureMutator
 ]
-
