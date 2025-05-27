3b8820284f152b534afaa6c61fa6fedbf26107a3
rh8588 <rh8588@kit.edu>
2025-05-27 17:12:10 +0200

    Add option to plot history to Optimizer base class

--- cnnparted/framework/optimizer/optimizer.py
+++ cnnparted/framework/optimizer/optimizer.py
@@ -60,19 +60,63 @@ class Optimizer():
     def _plot_history(self, res: Result, out_path: str):
         best_results = []
         all_results = []
+        all_constraints = []
 
+        n_obj = res.F.shape[-1]
+        n_constr = res.G.shape[-1]
         if res.history:
             for hist in res.history:
-                all_costs = [float(indiv.F) for indiv in hist.pop]
-                best_results.append(min(all_costs))
-                all_results.append(all_costs)
+                cost_at_time = []
+                constr_at_time = []
+                for ind in hist.pop:
+                    if np.max(ind.get("G")) > 0:
+                        cost_at_time.append( np.full((n_obj), np.nan) )
+                        constr_at_time.append( np.full((n_constr), np.nan) )
+                    else:
+                        cost_at_time.append(ind.get("F"))
+                        constr_at_time.append(ind.get("G"))
 
+                #best_results.append(min(all_costs))
+                all_results.append(cost_at_time)
+                best_results.append(np.nanmin(np.array(cost_at_time), axis=0))
+                all_constraints.append(constr_at_time)
+
+        plt.style.use("seaborn-v0_8-paper")
         fig, ax = plt.subplots(dpi=1200)
+        plt.gca().set_prop_cycle(color=plt.rcParams['axes.prop_cycle'].by_key()['color'][0:2]) 
         ax.set_xlabel("Generation")
         ax.set_ylabel("EDP")
+        platforms = ["Eyeriss", "Simba", "Gemmini"] #TODO
+
+        # Iterate over each generation
         for idx, res in enumerate(all_results): 
-            ax.scatter(np.full_like(res, idx, dtype=int).tolist(), res, color='b')
-        ax.plot(best_results, 'r+')
+            
+            # By default, res has shape (n_indiv, n_obj). We want one scatter-plot per platform so we have to transpose
+            results_of_generation = np.array(res).T
+            for p, platform_res in enumerate(results_of_generation):
+                ax.scatter(np.full_like(platform_res, idx, dtype=int).tolist(), platform_res, label=platforms[p])
+
+        # Mark optimal results in each generation
+        for idx, res in enumerate(best_results):
+            best_results_of_generation = np.array(res)
+            ax.scatter(np.full_like(best_results_of_generation, idx, dtype=int).tolist(), res,  label="Optimals", marker="+", color="r", linestyle="None")
+        ax.plot(best_results, 'r+', label="Optimals")
+
+        # Remove duplicate labels
+        handles, labels = plt.gca().get_legend_handles_labels()
+        dict_of_labels = dict(zip(labels, handles))
+        plt.legend(dict_of_labels.values(), dict_of_labels.keys())
+
         os.makedirs(os.path.join(out_path, "dse_results", "figures"), exist_ok=True)
         out_file = os.path.join(out_path, "dse_results", "figures", "history.png")
         fig.savefig(out_file)
+
+        # Dumpy all results of DSE history as npy file
+        os.makedirs(os.path.join(out_path, "dse_results", "data"), exist_ok=True)
+        npy_file_cost = os.path.join(out_path, "dse_results", "data", "history_data_cost.npy")
+        with open(npy_file_cost, "wb") as f:
+            np.save(f, np.array(all_results))
+
+        npy_file_constr = os.path.join(out_path, "dse_results", "data", "history_data_constr.npy")
+        with open(npy_file_constr, "wb") as f:
+            np.save(f, np.array(all_constraints))
