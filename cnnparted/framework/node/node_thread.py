b12081de8636a5c1fb95e903db19e5335a7ad214
rh8588 <rh8588@kit.edu>
2025-06-18 16:50:12 +0200

    Fix evaluation of multiple networks overwriting previous results

--- cnnparted/framework/node/node_thread.py
+++ cnnparted/framework/node/node_thread.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import copy
 import numpy as np
 
 from framework.module_thread_interface import ModuleThreadInterface
@@ -16,23 +17,6 @@ class NodeThread(ModuleThreadInterface):
         if not self.config:
             return
 
-        network = self.ga.networks[0] #TODO Multiple networks
-
-        # Select which simulator should be used
-        match self.config["evaluation"]["simulator"]:
-            case "timeloop":
-                layers = self.ga.get_timeloop_layers(network)
-                simulator = Timeloop(self.config, self.dse_system_config)
-            case "mnsim":
-                layers = self.ga.get_mnsim_layers(network)
-                simulator = MNSIMInterface(self.config, self.ga.workloads[network].input_size)
-            case "zigzag":
-                layers = []
-                simulator = Zigzag(self.config)
-            case _:
-                layers = []
-                simulator = GenericNode(self.config)
-
         # Check if design is DSE enabled
         top_k = -1
         metric = "edp"
@@ -43,24 +27,60 @@ class NodeThread(ModuleThreadInterface):
             metric = self.dse_system_config.get("optimization", "edp")
             top_k = int(self.dse_system_config.get("top_k", -1))
 
-        # Check if some previous results are available
-        fname_csv = simulator.set_workdir(self.work_dir, network, self.id)
-        if os.path.isfile(fname_csv):
-            self.stats = NodeResult.from_csv(fname_csv, network, self.config)
-            self.stats.prune_designs(top_k, metric)
-            return
+        node_result_networks = []
 
-        # Perform the actual evaluation
-        if self.acc_adaptor is None:
-            self.stats = simulator.run(network, layers)
-        else:
-            self.stats = simulator.run_from_adaptor(network, layers, self.acc_adaptor)
+        # Evaluate all networks
+        for network in self.ga.networks:
 
-        if self.save_results:
-            self.stats.to_csv(fname_csv)
+            # Select which simulator should be used
+            match self.config["evaluation"]["simulator"]:
+                case "timeloop":
+                    layers = self.ga.get_timeloop_layers(network)
+                    simulator = Timeloop(self.config, self.dse_system_config)
+                case "mnsim":
+                    layers = self.ga.get_mnsim_layers(network)
+                    simulator = MNSIMInterface(self.config, self.ga.workloads[network].input_size)
+                case "zigzag":
+                    layers = []
+                    simulator = Zigzag(self.config)
+                case _:
+                    layers = []
+                    simulator = GenericNode(self.config)
+
+
+            # Check if some previous results are available
+            # TODO Currently NodeResults is always overwritten with every iteration. Need a way to properly merge the results
+            fname_csv = simulator.set_workdir(self.work_dir, network, self.id)
+            if os.path.isfile(fname_csv):
+                stats = NodeResult.from_csv(fname_csv, network, self.config)
+            else:
+                # Perform the actual evaluation
+                if self.acc_adaptor is None:
+                    stats = simulator.run(network, layers)
+                else:
+                    stats = simulator.run_from_adaptor(network, layers, self.acc_adaptor)
+
+                if self.save_results:
+                    stats.to_csv(fname_csv)
+
+            node_result_networks.append(stats)
         
+        self.stats = self._merge_network_results(node_result_networks)
         self.stats.prune_designs(top_k, metric)
 
+
+    def _merge_network_results(self, network_results: list[NodeResult]):
+        final_res = copy.deepcopy(network_results[0])
+
+        if len(network_results) == 1:
+            return final_res
+        
+        for network_result in network_results[1:]:
+            for final_res_design, design in zip(final_res.designs, network_result.designs):
+                nw = design.get_networks()[0]
+                final_res_design.networks[nw] = copy.deepcopy(design.networks[nw])
+        
+        return final_res
 
     def _remove_file(self,file_path):
         if os.path.exists(file_path):
